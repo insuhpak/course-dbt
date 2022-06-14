a0ce78626fa36bb811310fd18ded26e88d0ed0b6
Insuh <ikpak@ucsc.edu>
2022-06-14 16:37:03 +0000

    removed fct_order_items_product_info

--- greenery/models/marts/greenery/intermediate/int_orders_order_items_product_info.sql
+++ greenery/models/marts/greenery/intermediate/int_orders_order_items_product_info.sql
@@ -6,7 +6,31 @@ with orders as (
 
 order_items_product_info as (
 
-    select * from {{ ref('fct_order_items_product_info') }}
+    -- with order_items as (
+
+    -- select * from {{ ref('stg_order_items') }}
+
+    -- ),
+
+    -- product_info as (
+
+    --     select * from {{ ref('stg_products') }}
+
+    -- )
+
+    select
+        order_items.order_id as order_id,
+        order_items.quantity as ordered_quantity_of_product,
+        product_info.name as product_name,
+        product_info.price as product_price,
+        product_info.inventory as product_inventory,
+        order_items.product_id as product_id
+
+    from {{ ref('stg_order_items') }} as order_items
+    left join {{ ref('stg_products') }} as product_info
+    on order_items.product_id = product_info.id
+
+    order by order_items.order_id, order_items.product_id, order_items.quantity
 
 )
 
